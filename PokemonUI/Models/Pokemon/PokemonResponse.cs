fbbe781b3d1b2df351adf1f438e10329d70221c9
Jesus Maury Abundez Cruz <AbundezCruzMaury@hotmail.com>
2025-12-22 09:50:09 -0600

    Se implementdo vista para el endpoint de buscar pokemon por nombre y respectivo servicio y dependencias

---
+++ PokemonUI/Models/Pokemon/PokemonResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonUI.Models.Pokemon
+{
+    public class PokemonResponse
+    {
+        public int Id { get; set; }   
+        public string Name { get; set; } = string.Empty;
+        public int Power { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Nature { get; set; } = string.Empty;
+    }
+}
